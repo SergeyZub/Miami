fcb0a59492f79a00b5564333c7ce5088a86516a9
Sergey Zub <vook1996@gmail.com>
2021-06-01 16:06:19 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const header = document.querySelector('.header__container');
+const page = document.querySelector('.page');
 const title = document.querySelector('.header__title');
 const phone = document.querySelector('.header__phone');
 const toggler = document.querySelector('.header__toggler');
@@ -8,6 +9,16 @@ const nav = document.querySelector('.nav');
 
 toggler.addEventListener('click', () => {
   header.classList.toggle('header__container--active');
+  page.classList.toggle('page--active');
+  title.classList.toggle('header__title--active');
+  phone.classList.toggle('header__phone--active');
+  toggler.classList.toggle('header__toggler--active');
+  nav.classList.toggle('nav--active');
+});
+
+nav.addEventListener('click', () => {
+  header.classList.toggle('header__container--active');
+  page.classList.toggle('page--active');
   title.classList.toggle('header__title--active');
   phone.classList.toggle('header__phone--active');
   toggler.classList.toggle('header__toggler--active');
